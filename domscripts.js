67fe36f7b18b3eed99a71a9596e738d620122aca
Manuel <yayinternets@gmail.com>
2021-11-01 17:41:42 -0400

    update restore toHTMLTable() function

--- domscripts.js
+++ domscripts.js
@@ -728,7 +728,9 @@ GSDS_disjointedRangeToAVO("A2;A2:B4;D4,E5:F5;G1:H2,H1-H9,L8,:B2, G8")
 // pending refactoring/TODO/tasks: getHelpfulDOMScripts - get rid of this or wat?
 // completed refactoring/TODO/tasks: I removed CQPify polyfill because "CQPify()" is now defined as "HTMLify() with server access".  Make this vibe/jive with NS and WP solutions?
 // pending refactoring/TODO/tasks: rename/polyfill CQPify to "CodePenify"?  Because that's basically all it is now, it's a first principles thinking/frameworking to carry to each system
-function HTMLLibrarify (sHeadList) { // consider refactoring this for domcontenloaded scripts?  also consider refactoring to allow async scripts (wtf that is?), also refactor from es5 to es6 i guess
+function HTMLLibrarify (sHeadList) { // HTMLLibrarify is essentially a <script src> and <link> generator
+  // consider refactoring this for domcontenloaded scripts?  also consider refactoring to allow async scripts (wtf that is?), also refactor from es5 to es6 i guess
+  // eg HTMLLibrarify("https://manueldelanda.github.io/datascripts.js;https://manueldelanda.github.io/domscripts.js;https://cdn.jsdelivr.net/lodash/4/lodash.min.js;https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css;https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css")
   if (Array.isArray(sHeadList)) { sHeadList = sHeadList.join("\n"); }
   sHeadList = sHeadList.replace(/\;/g, "\n");
   return sHeadList.split("\n")
@@ -736,6 +738,19 @@ function HTMLLibrarify (sHeadList) { // consider refactoring this for domcontenl
         .filter(oEl => !oEl.trim().match(/^\/\//) )
         .filter(oEl => oEl.trim().length > 0)
         .map(e => ((e.match(/\.js$/) ? `<script src="${e}"></script>`: `<link rel="stylesheet" href="${e}" type="text/css" />`)))
+        .join("\n"); 
+}
+function HTMLInjecify(sHeadList) { // HTMLInjectify() is essentially a smart wrapper for domLoadStyles_Link() and domLoadScripts_Link(), refactor to allow domLoadScripts_SCRIPT() and domLoadStyles_CSS() too?
+  // eg HTMLInjecify("https://manueldelanda.github.io/datascripts.js;https://manueldelanda.github.io/domscripts.js;https://cdn.jsdelivr.net/lodash/4/lodash.min.js;https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css;https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css")
+  // The following sScripts variable is minified scripts from domscripts nobrainer section
+  var sScripts = `domLoadStyles_Link=function(t){Array.isArray(t)||(t=[t]),t.forEach(function(t){var e=document.getElementsByTagName("head")[0],r=document.createElement("link");r.setAttribute("rel","stylesheet"),r.setAttribute("type","text/css"),r.setAttribute("href",t),e.appendChild(r)})},domLoadScripts_Link=function(t){Array.isArray(t)||(t=[t]),t.forEach(function(t){var e=document.getElementsByTagName("head")[0],r=document.createElement("script");r.setAttribute("src",t.trim()),e.appendChild(r)})};`
+  if (Array.isArray(sHeadList)) { sHeadList = sHeadList.join("\n"); }
+  sHeadList = sHeadList.replace(/\;/g, "\n");
+  return sScripts + "\n\n" + sHeadList.split("\n")
+        .filter(oEl => oEl.trim().match(/\.js$||\.css$/) )
+        .filter(oEl => !oEl.trim().match(/^\/\//) )
+        .filter(oEl => oEl.trim().length > 0)
+        .map(e => ((e.match(/\.js$/) ? `domLoadScripts_Link("${e}")`: `domLoadStyles_Link("${e}")`)))
         .join("\n"); 
 }
 function HTMLDOMContentLoadedLibrarify(sHeadList) { // for loading CDN scripts dynamically injecting into <script>
@@ -750,10 +765,9 @@ function HTMLDOMContentLoadedLibrarify(sHeadList) { // for loading CDN scripts d
     alert('loaded cdn scripts');
   });
 `;
-  return sCode;
+  return sCode.trim();
   // alertTextArea(sCode);
 }; function HTMLDOMContentLoadedLibrarifySample() { return 'HTMLDOMContentLoadedLibrarify("https://manueldelanda.github.io/datascripts.js;https://manueldelanda.github.io/domscripts.js;https://cdn.jsdelivr.net/lodash/4/lodash.min.js")'; }
-
 
 
 function HTMLDOMContentLoadedify(sScript) {
@@ -855,3 +869,148 @@ function HTMLify(aCQPRecordsOriented, bSansHTMLTag) {
 // I removed CQPify polyfill because "CQPify()" is now defined as "HTMLify() with server access"
 // HTMLify alone is itself just a pure vanilla datascript.js function
 // function CQPify(aCQPRecordsOriented) { return HTMLify(aCQPRecordsOriented) };
+
+// THE FOLLOWING CODE USED TO BE "domscripts.serversafe", but now its just part of datahtmlscripts.js
+// pseudocode for new domscript function - refactor convertOSRToHTMLTable, convertRecordsOrientedArrayToHTMLTable, convertValuesOrientedToHTMLTable into one solution? 
+toHTMLTable = function(aArrayOrObject, aColumns, sTableID) {
+  // refactor the two functions into datascripts
+  isRO = function(a) { return (Array.isArray(a) && !Array.isArray(a[0]) ); }; isRecordsOriented = function(a) { return isRO(a); }
+  isOSR = function(a) { return (!Array.isArray(a) && a.allcells != undefined); };
+
+  if (isVO(aArrayOrObject)) {
+    return convertValuesOrientedToHTMLTable(aArrayOrObject, aColumns, sTableID);
+  } else if (isOSR(aArrayOrObject)) {
+    return convertOSRToHTMLTable(aArrayOrObject, aColumns, sTableID);
+  } else if (isRO(aArrayOrObject)) {
+    return convertRecordsOrientedArrayToHTMLTable(aArrayOrObject, aColumns, sTableID);
+  }
+}
+convertRecordsOrientedArrayToHTMLTable = function (aRecordsOriented, aColumns, sTableID) {
+    // convertRecordsOrientedArrayToHTMLTable(aRecordsOriented)
+    function returnAllKeysAmongAllObjectsInRecordsOrientedArray(aRecordsOriented) { return aRecordsOriented.reduce(function(agg, oElement313) { agg = agg.concat(Object.keys(oElement313)); agg = unique(agg); return agg; }, []) }
+    if (sTableID == undefined) { sTableID = ""; }
+
+    if (aColumns == undefined) {
+        // var aColumns = Object.keys(aRecordsOriented[0]);
+                 aColumns = returnAllKeysAmongAllObjectsInRecordsOrientedArray(aRecordsOriented);
+    }
+    // gsws gsws_SDJOWholeForm A4 gscell columnA row4
+    sHTMLTable = "<table id='" + sTableID + "' class='RecordsOrientedArrayToHTML gsws gsws_" + sTableID + "' style='margin: 0 auto; text-align: center;'>" + aRecordsOriented.reduce(function(agg, oElement, iIndex) {
+        agg = agg + "<tr>" + aColumns.reduce(function(agg000, oElement000, iIndex000) {
+            var sCell = columnToLetter(iIndex000+1) + (iIndex+2);
+            var sClasses = "gsws gscell gsws_" + sTableID + " " + sCell + " row" + (iIndex+2) + " column" + columnToLetter(iIndex000+1) + " cellcolumn" + iIndex000;
+            agg000 = agg000 + "<td title='" + sCell + "' class='" + sClasses + "'>" + oElement[oElement000] + "</td>"; // style='text-align:left'
+            return agg000;
+        }, "") + "</tr>";
+        return agg;
+    }, 
+        "<tr>" + aColumns.reduce(function(agg001, oElement001, iIndex001) {
+            var sCell = columnToLetter(iIndex001+1) + "1";
+            var sClasses = "gsws gscell gsws_" + sTableID + " " + sCell + " row1 column" + columnToLetter(iIndex001+1) + " cellcolumn" + iIndex001;
+            // var sClasses = "gsws row1 column" + columnToLetter(iIndex001+1) + " cellcolumn" + iIndex001;
+            return agg001 + "<th title='" + sCell + "' class='" + sClasses + "'>" + oElement001 + "</th>"; // style='border-right: 1px solid black; border-left: 1px solid black;'
+        }, "") + "</tr>"
+    ) + "</table>";
+        return sHTMLTable;
+}
+
+convertValuesOrientedToHTMLTable = function(aValuesOriented, aColumns, sTableID) {
+    /// convertValuesOrientedToHTMLTable([[1,2,3,4],[0,0,0,0],[9,9,9,9]], undefined, "gswsvo")
+    function returnAllKeysAmongAllObjectsInRecordsOrientedArray(aRecordsOriented) { return aRecordsOriented.reduce(function(agg, oElement313) { agg = agg.concat(Object.keys(oElement313)); agg = unique(agg); return agg; }, []) }
+    if (sTableID == undefined) { sTableID = ""; }
+
+    if (aColumns == undefined) {
+        // var aColumns = Object.keys(aRecordsOriented[0]);
+                 // aColumns = returnAllKeysAmongAllObjectsInRecordsOrientedArray(aRecordsOriented);
+    }
+    // gsws gsws_SDJOWholeForm A4 gscell columnA row4
+    sHTMLTable = "<table id='" + sTableID + "' class='convertValuesOrientedToHTMLTable gsws gsws_" + sTableID + "' style='margin: 0 auto; text-align: center;'>" + aValuesOriented.reduce(function(agg, oElement, iIndex) {
+        agg = agg + "<tr>" + oElement.reduce(function(agg000, oElement000, iIndex000) {
+            //console.log(oElement);
+            var sCell = columnToLetter(iIndex000+1) + (iIndex+1);
+            var sClasses = "gsws gscell gsws_" + sTableID + " " + sCell + " row" + (iIndex+1) + " column" + columnToLetter(iIndex000+1) + " cellcolumn" + iIndex000;
+            agg000 = agg000 + "<td title='" + sCell + "' class='" + sClasses + "'>" + oElement000 + "</td>"; // style='text-align:left'
+            return agg000;
+        }, "") + "</tr>";
+        return agg;
+    }, "") + "</table>";
+    return sHTMLTable.replace(/ id=''/g, "");
+}
+convertRecordsOrientedArrayToExcelXML = function(aArray, aColumns) {
+  // convertRecordsOrientedArrayToExcelXML
+  // normalize aRecordsOriented
+  var aRecordsOriented = normalizeRecordsOriented(toValuesOriented(aArray));
+  aColumns = aColumns || undefined;
+  if (aColumns == undefined) {
+    var aColumns = Object.keys(aArray[0]);
+  }
+  function convertSingleArrayToExcelRow(aSingleArray) {
+    return "<Row>" + aSingleArray.reduce(function(agg000, oElement000, iIndex000) {
+      if (Array.isArray(oElement000)) { oElement000 = JSON.stringify(oElement000) }
+      oElement000 = oElement000.toString();
+      agg000 = agg000 + "<Cell><Data ss:Type=\"String\">" + oElement000.replace(/</g, "").replace(/>/g, "").replace(/\"/g, "").replace(/\n/g, " ") + "</Data></Cell>";
+      return agg000;
+    }, "") + "</Row>";
+  }
+  return '<?xml version="1.0"?><?mso-application progid="Excel.Sheet"?><Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" xmlns:html="http://www.w3.org/TR/REC-html40"><Worksheet ss:Name="Sheet1"><Table>' + aArray.reduce(function(agg, oElement, iIndex) { // Object.keys(oElement).map vs aColumns.map
+    return agg + convertSingleArrayToExcelRow(aColumns.map(function(oElement007){ return oElement[oElement007]}) );
+  }, convertSingleArrayToExcelRow(aColumns) ) + "</Table></Worksheet></Workbook>";
+}
+
+convertaRecordsOrientedToInputBoxesForm = function(oICIResponse, aFields) {  
+    if (aFields == undefined) { aFields = Object.keys(oICIResponse); }
+    return aFields.reduce(function(agg009, oElement009, iIndex009) {
+        if (oICIResponse[oElement009] == undefined) { oICIResponse[oElement009] = ""; }
+        sValue = oICIResponse[oElement009];
+        if (typeof(sValue) != "string") { sValue = JSON.stringify(sValue) }
+        if (oICIResponse[oElement009].toString().indexOf("\n") > -1) {
+            agg009 = agg009 + "<tr><td><b>" + oElement009 + ": </b></td><td><textarea rows='10' cols='30' class='inputtedObject' id='label_" + oElement009 + "' name='label_" + oElement009 + "' />" +  superhtmlEntities(sValue) + "</textarea></td>";
+        } else {
+            agg009 = agg009 + "<tr><td><b>" + oElement009 + ": </b></td><td><input style='width:100%' class='inputtedObject' type='text' id='label_" + oElement009 + "' name='label_" + oElement009 + "' value='" + superhtmlEntities(sValue) + "' /><td>";
+        }
+        return agg009;
+    }, "<table>") + "</table>"
+}
+GSDS_disjointedRangeToAVO = function(sA1Notation) { // this function is NOT FOR DOM, just string/data-only
+  if (sA1Notation.match(/\*/g)) { return "ERROR - ASTERISK functions are for domTable ONLY!" } else {
+    // this function single-handledly dismantles getGoogleSheetRange and getGoogleSheetRangeValuesOriented
+    sA1Notation = sA1Notation.replace(/\-/g, ":").replace(/,/g, ";"); // sanitize
+    a1DCells = unique(sA1Notation.split(";").map(function(oEl) {
+      oEl=oEl.trim();
+      if (oEl.match("^:")) { oEl = "A1" + oEl; }
+      if (oEl.match(":$")) { return "ERROR - ASTERISK IS ASSUMED HERE.";  }
+      if (oEl.indexOf("\:") > -1) { return getGoogleSheetRange(oEl); } else { return oEl; }
+    }).flat().sort(sortAlphaNum));
+    // determine lowest cell and highest cell
+    iHighestColumn = a1DCells.reduce(function(oAg, oEl) {
+      return ((oAg<letterToColumn(cellToColumn(oEl))) ? letterToColumn(cellToColumn(oEl)) : oAg) 
+    }, 0)
+    
+    iLowestColumn = a1DCells.reduce(function(oAg, oEl) {
+      return ((oAg>letterToColumn(cellToColumn(oEl))) ? letterToColumn(cellToColumn(oEl)) : oAg) 
+    }, iHighestColumn)
+    
+    iHighestRow = a1DCells.reduce(function(oAg, oEl) {
+      return ((oAg<parseInt(cellToRow(oEl))) ? parseInt(cellToRow(oEl)) : oAg) 
+    }, 0);
+    
+    iLowestRow = a1DCells.reduce(function(oAg, oEl) {
+      return ((oAg>parseInt(cellToRow(oEl))) ? parseInt(cellToRow(oEl)) : oAg) 
+    }, iHighestRow);
+    sExpansiverRange = columnToLetter(iLowestColumn) + iLowestRow + ":" + columnToLetter(iHighestColumn) + iHighestRow;
+    //console.log("Expansive Range - " + sExpansiverRange);
+    var a2DCells = getGoogleSheetRangeValuesOriented(sExpansiverRange);
+    // var a2DCells = getGoogleSheetRangeValuesOriented(a1DCells[0] + ":" + a1DCells.slice(-1)[0]); NO FUNCIONA
+    return a2DCells.map(function(oEl) { return oEl.map(function(oEl2) {
+      if (a1DCells.indexOf(oEl2) > -1) { return oEl2; }
+    }) }); 
+  }
+}
+GSDS_disjointedRangeToAVO.sample = function() { return 'GSDS_disjointedRangeToAVO("-A2;A2:B4; D4,E5:F5;G1:H2,H1-H9,L8,:B2, G8")' }
+GSDS_disjointedRangeToArray = function(sA1Notation) { return GSDS_disjointedRangeToAVO(sA1Notation).flat().filter(function(oEl) { return oEl; }) }
+
+toHTMLSelect=function(aArray, sClassList) { // refractor this to accept array of values vs array of objects (select id?)
+  // aArray = JSON.parse(JSON.stringify(aArray)); aArray.unshift
+  if (sClassList == undefined) { sClassList = "aArraySelect"; }
+  return "<select class='" + sClassList + "'><option></option>" + aArray.map(function(oElement) { return "<option>" + oElement + "</option>"; }).join("")+"</select>";
+}
