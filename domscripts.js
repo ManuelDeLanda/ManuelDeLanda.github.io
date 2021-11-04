1dac0aa1d1faf82911161115763488457a7ce189
Manuel <yayinternets@gmail.com>
2021-11-04 12:26:54 -0400

    update restore toHTMLTable() function

--- domscripts.js
+++ domscripts.js
@@ -1014,3 +1014,60 @@ toHTMLSelect=function(aArray, sClassList) { // refractor this to accept array of
   if (sClassList == undefined) { sClassList = "aArraySelect"; }
   return "<select class='" + sClassList + "'><option></option>" + aArray.map(function(oElement) { return "<option>" + oElement + "</option>"; }).join("")+"</select>";
 }
+
+
+function deconstructDOM(sType, bDelete50000) {
+    if (sType == undefined) {    
+        var aArray = Array.from(document.head.children).concat(Array.from(document.body.children));
+    } else if (sType == "head") {
+        var aArray = Array.from(document.head.children);
+    } else if (sType == "body") {
+        var aArray = Array.from(document.body.children);
+    }
+    var aReturn = aArray.map((o,i)=>{
+        if (o.nodeName == "SCRIPT") {
+            if (o.src && o.src.match(/js$/) ) {
+                // return { "head": o.outerHTML }
+                return { "library": o.src }
+            } else {
+                // if (o.innerHTML.trim().match(/document\.addEventListener\('DOMContentLoaded'\, \(event\) \=\> \{(.*)\}\)\;/)) {
+                if (o.outerHTML.trim().match(/addEventListener/)) {
+                    console.log("test")
+                }
+                return { "script": o.innerText }
+            }
+        } else if (o.nodeName == "LINK" && o.href.match(/css$/) ) {
+            return { "library": o.href };
+        } else if (o.nodeName == "STYLE") {
+            return { "css": o.innerText }
+        } else {
+            var bDontDelete50000 = !bDelete50000;
+                //if (bDelete50000) {
+                //    return {"script": ""};
+                //} else 
+            sText = o.outerHTML;
+            if (sText.length > 49000) {
+                try {
+                    sDeconstructionNotes = "yikes, @" + i + " - " + sText.length + " cannot be deconstructed into a gscell, but o has " + o.children.length + " children and can be deconstructed maybe? " + (Array.from(o.children).map(oo=>oo.children.length).join(",") );
+                    // o.children.map(oo=>console.log(oo.children));
+                } catch(e) { sDeconstructionNotes = ""; } 
+            }
+            oReturn = {};
+            oReturn[o.parentNode.nodeName.toLowerCase()] = (o.outerHTML.length > 49000 && bDelete50000 ? `<pre>${o.outerHTML.length} was removed per bDelete50000; sDeconstructionNotes = ${sDeconstructionNotes}</pre>` : o.outerHTML);
+            return oReturn;
+        }
+    }).flat();
+
+    // begin reduce
+    // consolidate library into single cell
+    aLibraries = [];
+    aReturn = aReturn.reduce((a,e,i) => { if (e.library) { aLibraries.push(e.library); } else { a=a.concat(e) }; return a; }, [])
+    aReturn[0].library = aLibraries.join("\n");
+
+    // remove blanks
+    aReturn = toRO(toVO(aReturn).filter(o=>o.join("")))
+    // end reduce
+
+    return aReturn;
+
+}
